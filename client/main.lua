0d52b0465075a8c189c602380f47652cf227790f
Sander Jochems <sanderjochems@hotmail.nl>
2020-09-24 13:48:02 +0200

    Add esx_advancedjail to client main

--- client/main.lua
+++ client/main.lua
@@ -266,6 +266,9 @@ function OpenPoliceActionsMenu()
 				{label = _U('put_in_vehicle'), value = 'put_in_vehicle'},
 				{label = _U('out_the_vehicle'), value = 'out_the_vehicle'},
 				{label = _U('fine'), value = 'fine'},
+				-- start esx_advancedjail
+				{label = _U('jail'), value = 'jail'},
+				-- end esx_advancedjail
 				{label = _U('unpaid_bills'), value = 'unpaid_bills'}
 			}
 
@@ -296,6 +299,10 @@ function OpenPoliceActionsMenu()
 						TriggerServerEvent('esx_policejob:OutVehicle', GetPlayerServerId(closestPlayer))
 					elseif action == 'fine' then
 						OpenFineMenu(closestPlayer)
+					-- start esx_advancedjail
+					elseif action == 'jail' then
+						JailMenu(GetPlayerServerId(closestPlayer))
+					-- end esx_advancedjail
 					elseif action == 'license' then
 						ShowPlayerLicense(closestPlayer)
 					elseif action == 'unpaid_bills' then
@@ -1574,3 +1581,113 @@ function ImpoundVehicle(vehicle)
 	ESX.ShowNotification(_U('impound_successful'))
 	currentTask.busy = false
 end
+
+-- start esx_advancedjail
+function JailMenu(player)
+	ESX.UI.Menu.CloseAll()
+	
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'jailmenu', {
+		title = _U('jail_menu'),
+		align = 'top-left',
+		elements = {
+			{label = _U('mission_row'), value = 'mission_row'},
+			{label = _U('bolingbroke_pen'), value = 'bolingbroke_pen'}
+	}}, function(data, menu)
+		if data.current.value == 'mission_row' then
+			ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'mission_row', {
+				title    = _U('mission_row'),
+				align    = 'top-left',
+				elements = {
+					{label = _U('mission_row_1'),	value = 'mission_row_1'},
+					{label = _U('mission_row_2'),	value = 'mission_row_2'},
+					{label = _U('mission_row_3'),	value = 'mission_row_3'}
+			}}, function(data2, menu2)
+				local action = data2.current.value
+
+				if action == 'mission_row_1' then
+					ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'jail_dialog', {
+						title = _U('jail_menu_info'),
+					}, function (data3, menu3)
+						local jailTime = tonumber(data3.value)
+
+						if jailTime == nil then
+							ESX.ShowNotification(_U('invalid_amount'))
+						else
+							TriggerServerEvent("esx_advancedjail:sendToJail", player, jailTime * 60, 'mr1')
+							ESX.UI.Menu.CloseAll()
+							menu3.close()
+						end
+					end, function (data3, menu3)
+						menu3.close()
+					end)
+				elseif action == 'mission_row_2' then
+					ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'jail_dialog', {
+						title = _U('jail_menu_info'),
+					}, function (data3, menu3)
+						local jailTime = tonumber(data3.value)
+
+						if jailTime == nil then
+							ESX.ShowNotification(_U('invalid_amount'))
+						else
+							TriggerServerEvent("esx_advancedjail:sendToJail", player, jailTime * 60, 'mr2')
+							ESX.UI.Menu.CloseAll()
+							menu3.close()
+						end
+					end, function (data3, menu3)
+						menu3.close()
+					end)
+				elseif action == 'mission_row_3' then
+					ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'jail_dialog', {
+						title = _U('jail_menu_info'),
+					}, function (data3, menu3)
+						local jailTime = tonumber(data3.value)
+
+						if jailTime == nil then
+							ESX.ShowNotification(_U('invalid_amount'))
+						else
+							TriggerServerEvent("esx_advancedjail:sendToJail", player, jailTime * 60, 'mr3')
+							ESX.UI.Menu.CloseAll()
+							menu3.close()
+						end
+					end, function (data3, menu3)
+						menu3.close()
+					end)
+				end
+			end, function(data2, menu2)
+				menu2.close()
+			end)
+		elseif data.current.value == 'bolingbroke_pen' then
+			ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'bolingbroke_pen', {
+				title    = _U('bolingbroke_pen'),
+				align    = 'top-left',
+				elements = {
+					{label = _U('bolingbroke_pen_1'),	value = 'bolingbroke_pen_1'}
+			}}, function(data2, menu2)
+				local action = data2.current.value
+
+				if action == 'bolingbroke_pen_1' then
+					ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'jail_dialog', {
+						title = _U('jail_menu_info'),
+					}, function (data3, menu3)
+						local jailTime = tonumber(data3.value)
+
+						if jailTime == nil then
+							ESX.ShowNotification(_U('invalid_amount'))
+						else
+							TriggerServerEvent("esx_advancedjail:sendToJail", player, jailTime * 60, 'bp1')
+							ESX.UI.Menu.CloseAll()
+							menu3.close()
+						end
+					end, function (data3, menu3)
+						menu3.close()
+					end)
+				end
+			end, function(data2, menu2)
+				menu2.close()
+			end)
+		end
+	end, function (data, menu)
+		menu.close()
+	end)
+end
+-- end esx_advancedjail
