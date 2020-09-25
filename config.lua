3a5d71e3ad7e653d340c3285e1eb4b43824401bd
Sander Jochems <sanderjochems@hotmail.nl>
2020-09-25 13:43:51 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -77,438 +77,6 @@ Config.PoliceStations = {
 			vector3(448.4, -973.2, 30.6)
 		}
 
-	},
-
-	VWPD = {
-	
-		Blip = {
-			Coords     = vector3(649.42, -10.37, 82.78),
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29
-		},
-	
-		Cloakrooms = {
-			vector3(649.42, -10.37, 82.78)
-		},
-	
-		Armories = {
-			vector3(638.93, 1.3, 82.79)
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(535.13, -22.27, 70.63),
-				InsideShop = vector3(228.5, -993.5, -99.5),
-				SpawnPoints = {
-					{coords = vector3(539.41, -47.97, 70.92), heading = 204.5, radius = 6.0},
-					{coords = vector3(543.16, -44.77, 70.86), heading = 204.5, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(602.55, 18.34, 87.91),
-				InsideShop = vector3(228.5, -993.5, -99.0),
-				SpawnPoints = {
-					{coords = vector3(626.32, 24.74, 87.88), heading = 248.3, radius = 6.0},
-					{coords = vector3(592.65, 36.05, 91.35), heading = 254.2, radius = 6.0}
-				}
-			}
-		},
-	
-		BossActions = {
-			vector3(643.97, 9.45, 82.79)
-		}
-	
-	},
-	
-	RHPD = {
-	
-		Blip = {
-			Coords     = vector3(-561.03, -132.09, 38.05), --rockford hills pd
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			vector3(-566.53, -133.03, 37.97)
-		},
-	
-		Armories = {
-			vector3(557.3, -129.53, 38)
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(-554.39, -136.78, 38.35),
-				InsideShop = vector3(228.5, -993.5, -99.0),
-				SpawnPoints = {
-					{coords = vector3(-563.65, -175.62, 38.09), heading = 16.7, radius = 6.0},
-					{coords = vector3(-555.26, -145.25, 38.16), heading = 116.7, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(-563.28, -140.38, 38.28),
-				InsideShop = vector3(228.5, -993.5, -99.0),
-				SpawnPoints = {
-					{coords = vector3(-564.35, -175.87, 38.08), heading = 37.9, radius = 6.0},
-					{coords = vector3(-558.1, -162.15, 38.17), heading = 290.8, radius = 6.0}
-				}
-			}
-		},
-	
-		BossActions = {
-			vector3(-561.03, -132.09, 38.05)
-		}
-	
-	},
-	
-	VEPD = {
-	
-		Blip = {
-			Coords     = vector3(-1081.2, -796.82, 19.28), --vespucci pd
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			vector3(-1117.95, 852.05, 19.69),
-		},
-	
-		Armories = {
-			vector3(-1122.99, -855.56, 19.69),
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(-1078.37, -857.1, 5.04),
-				SpawnPoints = {
-					{coords = vector3(-1042.76, -858.37, 4.89), heading = 62.9, radius = 6.0},
-					{coords = vector3(-1051.92, -867.1, 5.13), heading = 61.8, radius = 6.0},
-					{coords = vector3(-1069.63, -878.13, 4.85), heading = 34.2, radius = 6.0},
-					{coords = vector3(-1079.8, -883.99, 4.6), heading = 30.0, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(-1126.22, -833.73, 13.4),
-				SpawnPoints = {
-					{coords = vector3(-1117.06, -855.51, 13.5), heading = 44.8, radius = 6.0},
-					{coords = vector3(-1127.27, -863.5, 13.56), heading = 35.9, radius = 6.0}
-				}
-			}
-		},
-	
-		Helicopters = {
-			{
-				Spawner    = vector3(-1084.46, -837.46, 37.67),
-				SpawnPoint = vector3(-1096.4, -832.79, 37.68),
-				Heading    = 0.0
-			}
-		},
-	
-		VehicleDeleters = {
-			vector3(-1120.84, -843.66, 13.39),
-			vector3(-1124.56, -839.99, 13.4),
-			vector3(-1073.26, -856.06, 4.87),
-			vector3(-1069.07, -852.19, 4.87)
-		},
-	
-		BossActions = {
-			vector3(-1109.06, -845.92, 19.32)
-		}
-	
-	},
-	
-	VBPD = {
-	
-		Blip = {
-			Coords     = vector3(-1179.17, -1776.72, 3.91),
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			vector3(-1182.19, 1772.19, 3.91),
-		},
-	
-		Armories = {
-			vector3(-1177.47, -1780.42, 3.91),
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(-1171.15, -1757.34, 3.96),
-				SpawnPoints = {
-					{coords = vector3(-1177.13, -1744.19, 4.03), heading = 213.6, radius = 6.0},
-					{coords = vector3(-1170.87, -1739.61, 4.06), heading = 220.5, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(-1157.68, -1749.1, 4.03),
-				SpawnPoints = {
-					{coords = vector3(-1159.17, -1730.55, 4.16), heading = 210.2, radius = 6.0},
-					{coords = vector3(-1165.44, -1735.6, 4.1), heading = 210.7, radius = 6.0}
-				}
-			}
-		},
-	
-		Helicopters = {
-			{
-				Spawner    = vector3(-1182.19, 1772.19, 3.91),
-				SpawnPoint = vector3(-1179.99, 1719.11, 4.31),
-				Heading    = 0.0
-			}
-		},
-	
-		VehicleDeleters = {
-			vector3(-1167.96, -1771.35, 3.89),
-			vector3(-1169.96, -1767.9, 3.87)
-		},
-	
-		BossActions = {
-			vector3(-1179.17, -1776.72, 3.91)
-		}
-	
-	},
-	
-	DAPD = {
-	
-		Blip = {
-			Coords     = vector3(374.76, -1616.79, 29.29),
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			vector3(370.01, -1608.2, 29.29),
-		},
-	
-		Armories = {
-			vector3(371.93, -1611.98, 29.29),
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(384.82, -1634.83, 29.29),
-				SpawnPoints = {
-					{coords = vector3(394.49, -1625.7, 29.29), heading = 58.0, radius = 6.0},
-					{coords = vector3(399.18, -1621.22, 29.29), heading = 67.1, radius = 6.0},
-					{coords = vector3(392.95, -1608, 29.29), heading = 219.3, radius = 6.0},
-					{coords = vector3(388.67, -1612.8, 29.29), heading = 231.7, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(409.98, -1623.54, 29.29),
-				SpawnPoints = {
-					{coords = vector3(410.17, -1648.21, 29.29), heading = 242.7, radius = 6.0},
-					{coords = vector3(413.12, -1634.14, 29.29), heading = 143.6, radius = 6.0}
-				}
-			}
-		},
-	
-		Helicopters = {
-			{
-				Spawner    = vector3(0, 0, 0),
-				SpawnPoint = vector3(0, 0, 0),
-				Heading    = 0.0
-			}
-		},
-	
-		VehicleDeleters = {
-			vector3(380.79, -1629.81, 28.71),
-			vector3(377.97, -1627.22, 28.74)
-		},
-	
-		BossActions = {
-			vector3(374.76, -1616.79, 29.29)
-		}
-	
-	},
-	
-	LMPD = {
-	
-		Blip = {
-			Coords     = vector3(824.24, -1290.05, 28.24),
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			vector3(826.36, -1292.3, 28.24),
-		},
-	
-		Armories = {
-			vector3(826.48, -1287.86, 28.24),
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(863.98, -1337.76, 26.02),
-				SpawnPoints = {
-					{coords = vector3(852.98, -1368.63, 26.12), heading = 16.5, radius = 6.0},
-					{coords = vector3(831.73, -1359.89, 26.11), heading = 268.5, radius = 6.0},
-					{coords = vector3(821.96, -1340.91, 26.11), heading = 179.7, radius = 6.0},
-					{coords = vector3(850.8, -1345.54, 26.06), heading = 5.2, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(850.05, -1284.17, 28.00),
-				SpawnPoints = {
-					{coords = vector3(837.65, -1264.7, 26.34), heading = 92.4, radius = 6.0},
-					{coords = vector3(820.46, -1265.09, 26.23), heading = 93.7, radius = 6.0}
-				}
-			}
-		},
-	
-		Helicopters = {
-			{
-				Spawner    = vector3(0, 0, 0),
-				SpawnPoint = vector3(0, 0, 0),
-				Heading    = 0.0
-			}
-		},
-	
-		VehicleDeleters = {
-			vector3(855.53, -1281.81, 26.52),
-			vector3(871.77, -1350.19, 26.31),
-			vector3(823.86, -1372.81, 26.14),
-			vector3(832.45, -1372.91, 26.13)
-		},
-	
-		BossActions = {
-			vector3(824.24, -1290.05, 28.24)
-		}
-	
-	},
-	
-	SSPD = {
-	
-		Blip = {
-			Coords     = vector3(1855.2, 3683.25, 34.27),
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			vector3(1825.36, 3672.93, -118.76),
-		},
-	
-		Armories = {
-			vector3(1827.54, 3669.71, -118.76),
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(1857.27, 3688.75, 34.27),
-				SpawnPoints = {
-					{coords = vector3(1869.6, 3693.13, 33.63), heading = 215.8, radius = 6.0},
-					{coords = vector3(1858.84, 3711.88, 33.28), heading = 38.5, radius = 6.0},
-					{coords = vector3(1865.53, 3680.97, 33.64), heading = 239.2, radius = 6.0},
-					{coords = vector3(1866.7, 3661.21, 33.82), heading = 298, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(1853.99, 3701.53, 34.27),
-				SpawnPoints = {
-					{coords = vector3(1842.07, 3709.14, 33.39), heading = 14.8, radius = 6.0},
-					{coords = vector3(1822.85, 3700.93, 33.76), heading = 295.1, radius = 6.0}
-				}
-			}
-		},
-	
-		Helicopters = {
-			{
-				Spawner    = vector3(1730.73, 3607.64, 34.94),
-				SpawnPoint = vector3(1725.55, 3617.25, 36.95),
-				Heading    = 0.0
-			}
-		},
-	
-		VehicleDeleters = {
-			vector3(1877.35, 3697.21, 33.4),
-			vector3(1858.23, 3678.31, 33.71)
-		},
-	
-		BossActions = {
-			vector3(1844.76, 3672.02, -118.76)
-		},
-	
-	},
-	
-	PLPD = {
-	
-		Blip = {
-			Coords     = vector3(-442.38, 6017.26, 31.71),
-			Sprite  = 60,
-			Display = 4,
-			Scale   = 1.2,
-			Colour  = 29,
-		},
-	
-		Cloakrooms = {
-			{ x =  -425.02, y = 5990.1, z = -118.76 },
-		},
-	
-		Armories = {
-			vector3(-422.86, 5992.02, -118.76),
-		},
-	
-		Vehicles = {
-			{
-				Spawner    = vector3(-449.24, 6016.89, 31.72),
-				SpawnPoints = {
-					{coords = vector3(-452.25, 6049.56, 31.34), heading = 211.5, radius = 6.0},
-					{coords = vector3(-444.37, 6053.37, 31.34), heading = 211.5, radius = 6.0},
-					{coords = vector3(-437.87, 6040.77, 31.34), heading = 295.1, radius = 6.0},
-					{coords = vector3(-448.57, 6036.72, 31.34), heading = 312.3, radius = 6.0}
-				}
-			},
-	
-			{
-				Spawner    = vector3(-452.2, 6005.77, 31.84),
-				SpawnPoints = {
-					{coords = vector3(-474.08, 6022.38, 31.34), heading = 309.4, radius = 6.0},
-					{coords = vector3(-467.97, 6016.16, 31.34), heading = 312.4, radius = 6.0}
-				}
-			}
-		},
-	
-		Helicopters = {
-			{
-				Spawner    = vector3(-462.27, 5998.8, 31.3),
-				SpawnPoint = vector3(-482.62, 5996.44, 33.22),
-				Heading    = 0.0
-			}
-		},
-	
-		VehicleDeleters = {
-			vector3(-461.13, 6046.48, 31.34),
-			vector3(-455.26, 6048.48, 31.34)
-		},
-	
-		BossActions = {
-			vector3(-430.97, 6007, -118.76)
-		}
-	
 	}
 
 }
@@ -564,33 +132,33 @@ Config.AuthorizedVehicles = {
 		recruit = {},
 
 		officer = {
-			{model = 'police3', price = 20000},
-			{model = 'policeb', price = 30500}
+			{model = 'police3', price = 200},
+			{model = 'policeb', price = 305}
 		},
 
 		sergeant = {
-			{model = 'police2', price = 18500},
-			{model = 'police3', price = 20000},
-			{model = 'policeb', price = 30500},
-			{model = 'policet', price = 18500}
+			{model = 'police2', price = 185},
+			{model = 'police3', price = 200},
+			{model = 'policeb', price = 305},
+			{model = 'policet', price = 185}
 		},
 
 		lieutenant = {
-			{model = 'police2', price = 18500},
-			{model = 'police3', price = 20000},
-			{model = 'policeb', price = 30500},
-			{model = 'policet', price = 18500},
-			{model = 'riot', price = 70000},
+			{model = 'police2', price = 185},
+			{model = 'police3', price = 200},
+			{model = 'policeb', price = 305},
+			{model = 'policet', price = 185},
+			{model = 'riot', price = 700},
 		},
 
 		boss = {
-			{model = 'police', price = 15000},
-			{model = 'police2', price = 18500},
-			{model = 'police3', price = 20000},
-			{model = 'police4', price = 25000},
-			{model = 'policet', price = 18500},
-			{model = 'policeb', price = 30500},
-			{model = 'riot', price = 70000},
+			{model = 'police', price = 150},
+			{model = 'police2', price = 185},
+			{model = 'police3', price = 200},
+			{model = 'police4', price = 250},
+			{model = 'policet', price = 185},
+			{model = 'policeb', price = 305},
+			{model = 'riot', price = 700},
 		}
 	},
 
@@ -602,11 +170,11 @@ Config.AuthorizedVehicles = {
 		sergeant = {},
 
 		lieutenant = {
-			{model = 'polmav', props = {modLivery = 0}, price = 200000}
+			{model = 'polmav', props = {modLivery = 0}, price = 2000}
 		},
 
 		boss = {
-			{model = 'polmav', props = {modLivery = 0}, price = 100000}
+			{model = 'polmav', props = {modLivery = 0}, price = 2000}
 		}
 	}
 }
